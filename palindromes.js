1c9c3b461ab9d0ba3effc29d3c5f584ea20f2a26
inan yigit <inanygt@hotmail.com>
2022-08-15 18:16:31 +0200

    finished palindromes exc

--- palindromes.js
+++ palindromes.js
@@ -1,6 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function(string) {
+   let nString = string.toLowerCase().replace(/[^a-z]/g, "");
+   if (nString == nString.split("").reverse().join("")) {
+      return true;
+   } else {
+      return false;
+   }
 };
 
 // Do not edit below this line
 module.exports = palindromes;
+
+// npm test palindromes.spec
